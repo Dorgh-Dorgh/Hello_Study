6fbe9f7f64a0af3388fc8fbd0c2d6c00a9dd6610
hyj562 <hyj562@naver.com>
2026-01-08 02:32:37 +0900

    01.08_02:32

---
+++ apps/web/src/pages/App.jsx
@@ -0,0 +1,20 @@
+import "../App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <header className="header">
+        <div className="auth">
+          <button className="btn" type="button">회원가입</button>
+          <button className="btn" type="button">로그인</button>
+        </div>
+      </header>
+
+      <main className="main">
+        { }
+      </main>
+    </div>
+  );
+}
+
+export default App;
